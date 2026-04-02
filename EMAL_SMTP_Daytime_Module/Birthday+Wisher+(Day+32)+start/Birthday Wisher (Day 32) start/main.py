48cbaa46128d866e8dc911cb6ec0ca17f1b132d9
Adarsh Misra <workonprojects24@gmail.com>
2026-04-02 22:03:53 +0530

    Update main.py

--- EMAL_SMTP_Daytime_Module/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ EMAL_SMTP_Daytime_Module/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,28 +1,3 @@
-# Simple Main Transfer Protocol
-# import smtplib
-# my_email = "amisra_be23@thapar.edu"
-# my_password = "avjk mojn tosy ilzi"
-#
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=my_password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="yfan701@gamil.com",
-#         msg="Sub:Hello\n\nThis is the body of my email"
-#     )
-
-# import datetime as dt
-# now = dt.datetime.now()
-# year = now.year
-# month = now.month
-# day_of_week = now.weekday()
-# print(day_of_week)
-# print(now)
-#
-# date_of_birth = dt.datetime(year=2003, month=12, day=17, hour=11)
-# print(date_of_birth)
-
 import smtplib
 import random
 import datetime as dt
